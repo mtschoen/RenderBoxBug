6400eb20ba56a1a2a30eedef136046fe86a5d4bd
JChamon <jesuschamon7@gmail.com>
2017-09-07 12:00:39 +0200

    Project added

---
+++ app/src/main/java/com/chamon/renderboxbug/Activity1.java
@@ -0,0 +1,58 @@
+package com.chamon.renderboxbug;
+
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.cardbookvr.renderbox.IRenderBox;
+import com.cardbookvr.renderbox.RenderBox;
+import com.cardbookvr.renderbox.Transform;
+import com.cardbookvr.renderbox.components.Camera;
+import com.cardbookvr.renderbox.components.Sphere;
+import com.google.vr.sdk.base.GvrActivity;
+import com.google.vr.sdk.base.GvrView;
+
+public class Activity1 extends GvrActivity implements IRenderBox {
+    GvrView gvrView;
+
+    final int DEFAULT_BACKGROUND = R.drawable.bg;
+    Sphere photosphere;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_1);
+
+        gvrView = (GvrView) findViewById(R.id.gvr_view);
+
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+        gvrView.setRenderer(new RenderBox(this, this));
+        setGvrView(gvrView);
+
+        Toast.makeText(this,"Activity1",Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void setup() {
+        photosphere = new Sphere(DEFAULT_BACKGROUND, false);
+        new Transform().setLocalScale(Camera.Z_FAR * 0.99f, -Camera.Z_FAR * 0.99f, Camera.Z_FAR * 0.99f).addComponent(photosphere);
+
+    }
+
+    @Override
+    public void preDraw() {
+
+    }
+
+    @Override
+    public void postDraw() {
+
+    }
+
+    @Override
+    public void onCardboardTrigger(){
+        startActivity(new Intent(Activity1.this,Activity2.class));
+    }
+}
